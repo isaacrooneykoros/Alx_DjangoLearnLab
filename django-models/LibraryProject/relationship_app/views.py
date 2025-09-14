30f095f547fe46701a7f18e57489a3585bdd608a
isaacrooneykoros <isaacrooneykoros@gmail.com>
2025-09-14 18:47:54 +0300

    fix: update views.py to add from django.views.generic.detail import DetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
