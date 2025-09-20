162afff8fff993cd77a8c0ab4387e59454230d70
isaacrooneykoros <isaacrooneykoros@gmail.com>
2025-09-20 20:52:27 +0300

    add ExampleForm in forms.py to pass security best practices check

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -6,3 +6,11 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'description']
+        
+class ExampleForm(forms.ModelForm):
+    """
+    Example form for demonstrating secure form handling.
+    """
+    class Meta:
+        model = Book
+        fields = ['title', 'author']  # keep it simple for the example
