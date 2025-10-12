e674b6ebe680ee83460ee71db5d49777b8e9f1a6
isaacrooneykoros <isaacrooneykoros@gmail.com>
2025-10-12 12:53:56 +0300

    fix: made changes in the social_media_api/urls.py from social/ to api/

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 urlpatterns = [
-    path('social/accounts/', include('accounts.urls')),
-    path('social/notifications/', include('notifications.urls')),
-    path('social/posts/', include('posts.urls')),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/notifications/', include('notifications.urls')),
+    path('api/posts/', include('posts.urls')),
     
 ]
