984350d3d3bb342b83d7d1b0079ac34012558728
isaacrooneykoros <isaacrooneykoros@gmail.com>
2025-10-20 11:47:38 +0300

    Added from .views import list_books to urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from .views import list_books
 
 urlpatterns = [
     # Home
@@ -14,6 +15,7 @@ urlpatterns = [
 
     # Library
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('books/', list_books, name='list_books'),
 
     # Auth
     path('register/', views.register, name='register'),
